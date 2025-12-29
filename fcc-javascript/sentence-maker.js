8f7bba817c942569607016a74b6646db187c90a8
danielm-byte <danielmeshi1@gmail.com>
2025-12-29 22:04:53 +0100

    JavaScript mini projects to learn the fundamentals

---
+++ fcc-javascript/sentence-maker.js
@@ -0,0 +1,18 @@
+let adjective = 'big';
+let noun = 'dog';
+let verb = 'cold';
+let place = 'backyard';
+let adjective2 = 'beautiful';
+let noun2 = 'mice';
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+console.log("First story: " + firstStory);
+let secondStory 
+adjective = 'small';
+noun = 'cat';
+verb = 'sleepy';
+place = 'box';
+adjective2 = 'cute';
+noun2 = 'fish';
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
---
+++ fcc-javascript/trivia-bot.js
@@ -0,0 +1,13 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Dani";
+let botLocation  = "Earth";
+let favoriteLanguage   = "JavaScript";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact = favoriteLanguage;
+console.log(codingFact);
+codingFact = 'I love ' + favoriteLanguage;
+console.log(codingFact);
+codingFact = 'I love ' + favoriteLanguage + ", " + favoriteLanguage + " is the best!";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
