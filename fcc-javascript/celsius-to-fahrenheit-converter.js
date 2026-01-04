55d013c1e310a583cdfc63d1724ed646d2adfa3b
danielm-byte <danielmeshi1@gmail.com>
2026-01-04 19:51:18 +0100

    Add files via upload

---
+++ fcc-javascript/celsius-to-fahrenheit-converter.js
@@ -0,0 +1,13 @@
+function convertCtoF(num1) {
+return  num1 * (9/5) + 32
+}
+let num1 = -30;
+console.log(convertCtoF(num1));
+ num1 = -10;
+console.log(convertCtoF(num1));
+ num1 = 0;
+console.log(convertCtoF(num1));
+ num1 = 20;
+console.log(convertCtoF(num1));
+ num1 = 30;
+console.log(convertCtoF(num1));
